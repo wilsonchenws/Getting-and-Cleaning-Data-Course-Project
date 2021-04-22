107a04cd4eaac93a9c144aba0088073a9fa4b584
wilsonchenws <weishengchen765@gmail.com>
2021-04-23 00:21:25 +0800

    1.Merge testing and training set first to save code lines.
    2.Use melt and dcast to reduce numbers of rows. (Calculating average of each subject and  activity.)

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(reshape2)
 # Read features(X_traing), activity(Y_train), subject for training set.
 train_data_df <- read.table("./UCI HAR Dataset/train/X_train.txt",header = F)
 train_subject_df <- read.table("./UCI HAR Dataset/train/subject_train.txt",header = F)
@@ -13,14 +13,20 @@ test_activity_df <- read.table("./UCI HAR Dataset/test/y_test.txt",header = F)
 #Read feature list and identify measurement of mean and standard deviation.
 feature_info <- read.table("./UCI HAR Dataset/features.txt")
 feature_index <- grep("(mean|std)",feature_info[[2]])
-name_list <- feature_info[feature_index,2]
+#name_list <- feature_info[feature_index,2]
 
-# Use select function to select necessary column (mean and std),
-# Use mutate function to add activity column and subject column, also
-# Recode function to rename activity value into more descriptive name.
-train_df <- train_data_df %>%
-            select(all_of(feature_index)) %>%
-            mutate(SUBJECT = train_subject_df$V1,activity = recode_factor(train_activity_df$V1,
+#Merge dataset first:
+
+df.afterMerge <- rbind(cbind(test_data_df,test_subject_df),
+                  cbind(train_data_df,train_subject_df))
+names(df.afterMerge) <- c(feature_info$V2,"SUBJECT")
+#activity is named seperately to use mutate plus recode_factor function.+
+df.activity <- rbind(test_activity_df,train_activity_df)
+
+
+df.beforefinal <- df.afterMerge %>%
+            select(all_of(feature_index),562) %>%
+            mutate(ACTIVITY = recode_factor(df.activity[[1]],
                                             '1' = "WALKING",
                                             '2' = "WALKING_UPSTAIRS",
                                             '3' = "WALKING_DOWNSTAIRS",
@@ -28,26 +34,11 @@ train_df <- train_data_df %>%
                                             '5' = "STANDING",
                                             '6' = "LAYING"
                                             ))
-#reorder column sequence (Making subject and activty 1st and 2nd column)
-train_df <- train_df[,c(80,81,1:79)]
 
-#repeat above code for test_set
-test_df <- test_data_df %>%
-           select(all_of(feature_index)) %>%
-           mutate(SUBJECT = test_subject_df$V1,activity = recode_factor(test_activity_df$V1,
-                                            '1' = "WALKING",
-                                            '2' = "WALKING_UPSTAIRS",
-                                            '3' = "WALKING_DOWNSTAIRS",
-                                            '4' = "SITTING",
-                                            '5' = "STANDING",
-                                            '6' = "LAYING"
-    ))
-#reorder column sequence (Making subject and activty 1st and 2nd column)
-test_df <- test_df[,c(80,81,1:79)]
-#Merge testing test and training set
-final_df <- rbind(train_df,test_df)
-#Give Columns meaningful name
-names(final_df) <- c(c("SUBJECT","ACTIVITY"),name_list)
+#Melt the dataframe into a long format then recast it and average out.
+df.melted <- melt(df.beforefinal,id = c("SUBJECT","ACTIVITY"))
+df.final <- dcast(df.melted, SUBJECT+ACTIVITY ~ variable,fun.aggregate = mean)
 
-#(Optional) Output dataframe as a csv file.
-write.csv(final_df,"./Result.csv",row.names = F)
+
+# Output dataframe as a text file.
+write.table(final_df,"./Final_Dataset.txt",row.names = F)
